27e12a2cafdbff3e1b45444c6432b60ebc58a6d7
Mingeun Park <mindal99546@gmail.com>
2023-11-14 13:42:37 +0900

    refactor: 클래스 이름 변경
    before: ExpectedVisitDate
    after: VisitDate

--- src/main/java/christmas/domain/order/VisitDate.java
+++ src/main/java/christmas/domain/order/VisitDate.java
@@ -5,20 +5,20 @@ import christmas.exceptions.InValidVisitDateException;
 import java.time.Month;
 import java.time.MonthDay;
 
-public class ExpectedVisitDate {
+public class VisitDate {
 
     private static final Month EVENT_MONTH = Month.DECEMBER;
     private static final Integer FIRST_DAY = 1;
     private static final Integer LAST_DAY = 31;
     private MonthDay date;
 
-    private ExpectedVisitDate(Month month, int day) {
+    private VisitDate(Month month, int day) {
         date = MonthDay.of(month, day);
     }
 
-    public static  ExpectedVisitDate of(PureNumber day) throws InValidVisitDateException {
+    public static VisitDate of(PureNumber day) throws InValidVisitDateException {
         validateVisitDate(day.getNumber());
-        return new ExpectedVisitDate(EVENT_MONTH, day.getNumber());
+        return new VisitDate(EVENT_MONTH, day.getNumber());
     }
 
     private static void validateVisitDate(int day) throws InValidVisitDateException {
--- src/test/java/christmas/domain/VisitDateTest.java
+++ src/test/java/christmas/domain/VisitDateTest.java
@@ -3,29 +3,29 @@ package christmas.domain;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import christmas.IO.PureNumber;
-import christmas.domain.order.ExpectedVisitDate;
+import christmas.domain.order.VisitDate;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-public class ExpectedVisitDateTest {
+public class VisitDateTest {
 
     @DisplayName("유효하지 않은 방문 예상 날짜 입력 -> 예외 발생")
     @ParameterizedTest
     @CsvSource({"2k", "0", "2 2", "32", "100"})
-    void invalidExpectedDate(String input) {
+    void invalidVisitDate(String input) {
         assertThatThrownBy(
-                () -> ExpectedVisitDate.of(PureNumber.wrap(input))
+                () -> VisitDate.of(PureNumber.wrap(input))
         ).isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("유효한 방문 예상 날짜")
     @ParameterizedTest
     @CsvSource({"1", "16", "31"})
-    void validExpectedDate(String input) {
+    void validVisitDate(String input) {
         Assertions.assertDoesNotThrow(
-                () -> ExpectedVisitDate.of(PureNumber.wrap(input))
+                () -> VisitDate.of(PureNumber.wrap(input))
         );
     }
 }
